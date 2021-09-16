0cfe249cdd9fb16382129799874c2d38842f540c
Ivan Mikec <mikecivan0@gmail.com>
2021-09-16 15:17:43 +0200

    added base project

---
+++ src/main/java/hr/mikec/restwebapp/ResTwebAppApplication.java
@@ -0,0 +1,13 @@
+package hr.mikec.restwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTwebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTwebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/mikec/restwebapp/ResTwebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.mikec.restwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTwebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
